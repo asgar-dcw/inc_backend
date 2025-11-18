63c72f7b461dc76ed390b6b0d9b2782b8371444e
GoodMind India <info@goodmind.app>
2025-11-18 16:49:52 +0530

    Use .js extension in server import

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { testConnection } from './services/database';
+import { testConnection } from './services/database.js';
 
 // Import routes
 import salesRoutes from './routes/sales';
